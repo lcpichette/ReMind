464ccbec32388be69710fa7dbb1513b5ae02a1bf
Lucas Pichette <lucascpichette@gmail.com>
2023-12-30 05:03:03 -0800

    Removes codepipeline and adds EC2

--- iac/main.tf
+++ iac/main.tf
@@ -30,283 +30,29 @@ resource "aws_s3_bucket_acl" "example" {
   acl    = "public-read"
 }
 
-# CODE PIPELINE
+# DEV EC2
 
-resource "aws_codepipeline" "codepipeline" {
-  name     = "ReMind-codepipeline"
-  role_arn = aws_iam_role.codepipeline_role.arn
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-  artifact_store {
-    location = aws_s3_bucket.codepipeline_bucket.bucket
-    type     = "S3"
-
-    encryption_key {
-      id   = data.aws_kms_alias.s3kmskey.arn
-      type = "KMS"
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
-  stage {
-    name = "Source"
-
-    action {
-      name             = "Source"
-      category         = "Source"
-      owner            = "AWS"
-      provider         = "CodeStarSourceConnection"
-      version          = "1"
-      output_artifacts = ["source_output"]
-
-      configuration = {
-        ConnectionArn    = aws_codestarconnections_connection.example.arn
-        FullRepositoryId = "my-organization/example"
-        BranchName       = "main"
-      }
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 
-  stage {
-    name = "Build"
-
-    action {
-      name             = "Build"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["source_output"]
-      output_artifacts = ["build_output"]
-      version          = "1"
-
-      configuration = {
-        ProjectName = "ReMind"
-      }
-    }
-  }
-
-  stage {
-    name = "Deploy"
-
-    action {
-      name            = "Deploy"
-      category        = "Deploy"
-      owner           = "AWS"
-      provider        = "CloudFormation"
-      input_artifacts = ["build_output"]
-      version         = "1"
-
-      configuration = {
-        ActionMode     = "REPLACE_ON_FAILURE"
-        Capabilities   = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
-        OutputFileName = "CreateStackOutput.json"
-        StackName      = "ReMind-Stack"
-        TemplatePath   = "build_output::sam-templated.yaml"
-      }
-    }
-  }
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_codestarconnections_connection" "example" {
-  name          = "example-connection"
-  provider_type = "GitHub"
-}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
-resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket = "ReMind-codepipeline-bucket"
-}
-
-resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
-  bucket = aws_s3_bucket.codepipeline_bucket.id
-  acl    = "private"
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["codepipeline.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
+  tags = {
+    Name = "ReMind"
   }
-}
-
-resource "aws_iam_role" "codepipeline_role" {
-  name               = "ReMind-codepipeline-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-data "aws_iam_policy_document" "codepipeline_policy" {
-  path = "/"
-
-  statement {
-    Action : [
-      "iam:PassRole"
-    ],
-    Resource : "*",
-    Effect : "Allow",
-    Condition : {
-      "StringEqualsIfExists" : {
-        "iam:PassedToService" : [
-          "cloudformation.amazonaws.com",
-          "elasticbeanstalk.amazonaws.com",
-          "ec2.amazonaws.com",
-          "ecs-tasks.amazonaws.com"
-        ]
-      }
-    }
-  }
-
-  statement {
-    Action : [
-      "codecommit:CancelUploadArchive",
-      "codecommit:GetBranch",
-      "codecommit:GetCommit",
-      "codecommit:GetUploadArchiveStatus",
-      "codecommit:UploadArchive"
-    ],
-    Resource : "*",
-    Effect : "Allow"
-  }
-
-  statement {
-    Action : [
-      "codedeploy:CreateDeployment",
-      "codedeploy:GetApplication",
-      "codedeploy:GetApplicationRevision",
-      "codedeploy:GetDeployment",
-      "codedeploy:GetDeploymentConfig",
-      "codedeploy:RegisterApplicationRevision"
-    ],
-    Resource : "*",
-    Effect : "Allow"
-  }
-
-  statement {
-    Action : [
-      "elasticbeanstalk:*",
-      "ec2:*",
-      "elasticloadbalancing:*",
-      "autoscaling:*",
-      "cloudwatch:*",
-      "s3:*",
-      "rds:*"
-    ],
-    Resource : "*",
-    Effect : "Allow"
-  }
-
-  statement {
-    Action : [
-      "opsworks:CreateDeployment",
-      "opsworks:DescribeApps",
-      "opsworks:DescribeCommands",
-      "opsworks:DescribeDeployments",
-      "opsworks:DescribeInstances",
-      "opsworks:DescribeStacks",
-      "opsworks:UpdateApp",
-      "opsworks:UpdateStack"
-    ],
-    Resource : "*",
-    Effect : "Allow"
-  }
-
-  statement {
-    Action : [
-      "cloudformation:CreateStack",
-      "cloudformation:DeleteStack",
-      "cloudformation:DescribeStacks",
-      "cloudformation:UpdateStack",
-      "cloudformation:CreateChangeSet",
-      "cloudformation:DeleteChangeSet",
-      "cloudformation:DescribeChangeSet",
-      "cloudformation:ExecuteChangeSet",
-      "cloudformation:SetStackPolicy",
-      "cloudformation:ValidateTemplate"
-    ],
-    Resource : "*",
-    Effect : "Allow"
-  }
-
-  statement {
-    Action : [
-      "codebuild:BatchGetBuilds",
-      "codebuild:StartBuild"
-    ],
-    Resource : "*",
-    Effect : "Allow"
-  }
-
-  statement {
-    Effect : "Allow",
-    Action : [
-      "servicecatalog:ListProvisioningArtifacts",
-      "servicecatalog:CreateProvisioningArtifact",
-      "servicecatalog:DescribeProvisioningArtifact",
-      "servicecatalog:DeleteProvisioningArtifact",
-      "servicecatalog:UpdateProduct"
-    ],
-    Resource : "*"
-  }
-
-  statement {
-    Effect : "Allow",
-    Action : [
-      "cloudformation:ValidateTemplate"
-    ],
-    Resource : "*"
-  }
-
-  statement {
-    Effect : "Allow",
-    Action : [
-      "ecr:DescribeImages"
-    ],
-    Resource : "*"
-  }
-
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:GetBucketVersioning",
-      "s3:PutObjectAcl",
-      "s3:PutObject",
-    ]
-
-    resources = [
-      aws_s3_bucket.codepipeline_bucket.arn,
-      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-    ]
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["codestar-connections:UseConnection"]
-    resources = [aws_codestarconnections_connection.example.arn]
-  }
-
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "codebuild:BatchGetBuilds",
-      "codebuild:StartBuild",
-    ]
-
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_role_policy" "codepipeline_policy" {
-  name   = "codepipeline_policy"
-  role   = aws_iam_role.codepipeline_role.id
-  policy = data.aws_iam_policy_document.codepipeline_policy.json
-}
-
-data "aws_kms_alias" "s3kmskey" {
-  name = "alias/myKmsKey"
 }
